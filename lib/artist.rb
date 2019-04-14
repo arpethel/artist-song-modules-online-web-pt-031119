b7d36a2bbfcdb6c0dc1dbb55c5ff02cab4cc388f
Ryan Pethel <arpethel@gmail.com>
2019-04-14 15:47:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Artist
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
-  extend Findable
+  # extend Findable
   include Paramable
 
   attr_accessor :name
